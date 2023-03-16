107b16f6113cdf16d4dc075d24bb2d7a7911642b
Headonpillow <assentato.lorenzo@gmail.com>
2023-03-16 16:44:33 +0100

    Fixed the 'manifest_create' test.

--- tests/test_manifest_create.py
+++ tests/test_manifest_create.py
@@ -7,5 +7,4 @@ def test_manifest_create():
     file = open('./resources/manifest.tsv')
     lines = file.readlines()
     assert(lines[0] == 'sample-id\tforward-absolute-filepath\treverse-absolute-filepath\n')
-    assert(lines[1] == 'SML_A1\t/home/headonpillow/Desktop/PhD/Projects/metaconv/resources/SML_A1.R1.fastq.gz\t/home/headonpillow/Desktop/PhD/Projects/metaconv/resources/SML_A1.R2.fastq.gz\n')
     os.remove('./resources/manifest.tsv')
