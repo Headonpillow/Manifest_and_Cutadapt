73ad57759c2381356c25a9fe37e791943df6e3ea
Headonpillow <assentato.lorenzo@gmail.com>
2023-03-17 14:15:00 +0100

    Fixed documentation ad added comments for the code in the two modules.

--- metaconv/format_functions.py
+++ metaconv/format_functions.py
@@ -22,33 +22,54 @@ def mothur_to_cutadapt(string_list, adapter_type='^', barcode_length=7):
         Default `barcode_length = 7`
     '''
 
+    # Create two empty lists of strings, they will in the end have the whole
+    # content of the file, line by line.
     new_string_list_forward = []
     new_string_list_reverse = []
 
     for string in string_list:
+        # This excludes the "PRIMERS" string and only selects the barcodes.
         if 'BARCODE' not in string.upper():
             continue
         else:
+            # Two new line list are created, one forward and one reverse.
+            # The whole function runs in parallel creating contents for the
+            # two different files.
             new_line_forward = []
             new_line_reverse = []
 
+            # This part of the function takes care of setting an offset to the
+            # hard coded positions in the string below contained in the "else".
+            # This behaviour is needed because the function has been thought
+            # to deal with 7bp long barcodes, if the barcodes are longer, the index
+            # just gets updated.
+            # Still the function doesn't take into account the chance the barcode
+            # length might be shorter than 7.
             if barcode_length > 7:
+                # Calculates offset length
                 offset = barcode_length - 7
                 corrected_fwd_end = 15 + offset
                 corrected_rv_start = corrected_fwd_end + offset
                 corrected_rv_end = corrected_rv_start + barcode_length
                 corrected_sample_start = corrected_rv_end + offset
 
+                # Determines which string is forward and reverse and which one
+                # indicates the sample.
                 forward = string[8:corrected_fwd_end]
                 reverse = string[corrected_rv_start:corrected_rv_end]
                 sample = string[corrected_sample_start:]
 
             else:
+                # Hardcoded case in which the barcode is 7bp long, this is also
+                # the default behaviour of the function.
                 forward = string[8:15]
                 reverse = string[16:23]
                 sample = string[24:]
 
+            # Any occurrence of "/" is replaced with "-" for consistency.
             sample = sample.replace("/", "-")
+            # The new line is now created, with the specified "adapter_type"
+            # "new_line" is a list of strings.
             new_line_forward.append('>')
             new_line_forward.append(sample)
             new_line_forward.append(adapter_type)
@@ -61,11 +82,14 @@ def mothur_to_cutadapt(string_list, adapter_type='^', barcode_length=7):
             new_line_reverse.append(reverse)
             new_line_reverse.append('\n')
 
+            # A complete string is created from the "new_line" list and put it into a variable.
             new_string_forward = ''.join(new_line_forward)
+            # This new string is appended to the empty "content" list created outside the loop.
             new_string_list_forward.append(new_string_forward)
             new_string_reverse = ''.join(new_line_reverse)
             new_string_list_reverse.append(new_string_reverse)
 
+    # The function returns the content of forward and reverse files as a list of strings.
     return [new_string_list_forward, new_string_list_reverse]
 
 
@@ -93,15 +117,25 @@ def convert_oligos(path='.', adapter_type='^', barcode_length=7):
         Specify the length of the barcodes in the oligo files.
         Default `barcode_length = 7`
     """
+
+    # Retrieve the root from where the script is running from to change it back
+    # at the end of the function.
     root = os.getcwd()
+    # Change working directory to the path passed to the function where the files are.
     os.chdir(path)
 
     for oligo in glob.glob('*.file'):
         file = open(oligo)
+        # Read the lines of the oligo file and creates a list, this will be
+        # passed to the "mothur_to_cutadapt" function.
         string_list = file.readlines()
 
+        # Use the mothur_to_cutadapt() function to do the conversion.
+        # The content variable is a list, with one content forward and
+        # one content reverse.
         content = mothur_to_cutadapt(string_list, adapter_type, barcode_length)
 
+        # Write the contents in the new files
         file = open(oligo + '_cutadapt_f.fasta', "w")
         new_content = ''.join(content[0])
         file.write(new_content)
--- metaconv/manifest_create.py
+++ metaconv/manifest_create.py
@@ -2,47 +2,85 @@ import os
 
 
 def read_folder_content(raw_files_path='.'):
+    '''
+    It reads the content of a folder to retrieve a list of `.fastq.gz` files.
+
+    Parameters
+    ----------
+    raw_files_path :
+        The path where the raw `.fastq.gz` files are stored.
+    '''
+
+    # Change directory to match the "raw_files_path" specified.
     os.chdir(raw_files_path)
+    # Create an empty list to which append the names of the files in the folder.
     file_list = []
+    # Actually reads the content of the folder.
     content = os.listdir()
     for file in content:
+        # Appends to the "file_list" just the ".fastq.gz" files
         if '.fastq.gz' in file:
             file_list.append(os.path.abspath(file))
     return file_list
 
 
 def write_manifest(raw_files_path='.'):
+    '''
+    It writes a manifest file to use as import with Qiime2 from a folder of ´.fastq.gz'files.
+    The content of the folder is read and all the ´.gz´ files included in the manifest.
+    The manifest file can then be used normally with Qiime2 to start the analysis pipeline.
+
+    Parameters
+    ----------
+    raw_files_path :
+        The path where the raw `.fastq.gz` files are stored.
+    '''
+
+    # Sets the root from which the script is running from.
     root = os.getcwd()
-    # Use the function 'ReadFolderContent' to
+    # Use the function "read_folder_content" to get a list of the filenames in the folder.
     file_list = read_folder_content(raw_files_path)
+    # Creates a new list to which append the lines that will be created
     new_string_list = []
+    # The header is gonna be the same for all the created manifest files.
     header = 'sample-id\tforward-absolute-filepath\treverse-absolute-filepath\n'
     new_string_list.append(header)
     for string in file_list:
+        # This select just one string of the filenames, just the forward one.
+        # This happens because we just need one occurrence that will be adapted
+        # to write also the reverse one, but in this way we keep only one string to work on.
         if 'R1' in string:
-            # Create an empty new line variable to append the parts to.
+            # Create an empty new line variable to append the different part of the line.
             new_line = []
 
             # Get the path directory from the system and determine how long it is.
+            # This is needed to actually correctly index the subsequent string and retrieve
+            # just the sample name .
             path = os.getcwd() + '/'
             path_length = len(path)
 
-            # The length is needed to index the subsequent string and retrieve just the sample name.
             # The subset of the string starts when the path ends and stops at -12 (that removes the
             # last characters '.R1.fastq.gz').
             sample = string[path_length:-12]
+
+            # Here we start appending things to the "new_line".
             new_line.append(sample)
             new_line.append('\t')
             new_line.append(string)
             new_line.append('\t')
+            # And here the string is modified to consider the reverse file "R2".
             string2 = string.replace('R1', 'R2')
             new_line.append(string2)
             new_line.append('\n')
 
+            # A whole string is created with "join" and appended to the new list of strings
+            # that will be the content of the file at the end.
             new_string = ''.join(new_line)
             new_string_list.append(new_string)
 
+    # A file is open in writing mode and the new content is joined and written to it.
     file = open('manifest.tsv', "w")
     new_content = ''.join(new_string_list)
     file.write(new_content)
+    # The directory is changed back to root.
     os.chdir(root)
